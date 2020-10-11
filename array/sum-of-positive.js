e5aa100d63c75615253e972fc310f6adcd1646b8
am4ora <69114420+am4ora@users.noreply.github.com>
2020-10-11 23:25:38 +0300

    Update sum-of-positive.js

--- array/sum-of-positive.js
+++ array/sum-of-positive.js
@@ -18,9 +18,7 @@ function positiveSum(arr) {
     let result = 0;
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > 0) {
-            result = result + arr[i];
+           result = result + arr[i];
         }
-    } console.log(result);
+    } return result;
 }
-
-positiveSum([]);
